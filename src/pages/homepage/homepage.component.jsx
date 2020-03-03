4a885bad574082bfc3ba6d35819b46836b8a29ad
saviosid <savio_furtado@hotmail.com>
2020-03-03 17:42:45 +0530

    broken homepage into components and completed menu item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => {
+   return (
+      <div classname='homepage'>
+         <Directory />
+      </div>
+   );
+};
+export default HomePage;
