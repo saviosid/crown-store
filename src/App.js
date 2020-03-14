2e7443ea18960e2d0be0c92ac32ff8c28d172869
saviosid <savio_furtado@hotmail.com>
2020-03-14 13:31:27 +0530

    after Shop Page and Collection Preview and Collection Item

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import {Route, Switch} from 'react-router-dom'
-
-const HatsPage = () => (
-   <div>
-      <h1>HATS PAGE</h1>
-   </div>
-)
+import ShopPage from './pages/shop/shop.component';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
    return (
       <div className='App'>
          <Switch>
-            <Route exact path='/' component={HomePage}/>
-            <Route path='/hats' component={HatsPage}/>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
          </Switch>
-         
       </div>
    );
 }
